fc0a84f5686e004257a835188cd4d2f8277c85c6
Eugene Lanets <swarrt@yandex.ru>
2023-05-17 23:05:05 +0300

    feat: remove item selection

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -9,12 +9,6 @@ function Item(props){
   const [count, setCount] = useState(0);
 
   const callbacks = {
-    onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
     onDelete: (e) => {
       e.stopPropagation();
       props.onDelete(props.item.code);
@@ -22,11 +16,10 @@ function Item(props){
   }
 
   return (
-    <div className={'Item' + (props.item.selected ? ' Item_selected' : '')}
-         onClick={callbacks.onClick}>
+    <div className='Item'>
       <div className='Item-code'>{props.item.code}</div>
       <div className='Item-title'>
-        {props.item.title} {count ? ` | Выделяли ${count} ${plural(count, {one: 'раз', few: 'раза', many: 'раз'})}` : ''}
+        {props.item.title}
       </div>
       <div className='Item-actions'>
         <button onClick={callbacks.onDelete}>
