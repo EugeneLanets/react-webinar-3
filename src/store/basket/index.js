59b05c122cfe1c439613c3aa96dfec51243636e0
Eugene Lanets <swarrt@yandex.ru>
2023-05-26 22:25:40 +0300

    fix: remove unneccesary console.log

--- src/store/basket/index.js
+++ src/store/basket/index.js
@@ -14,7 +14,6 @@ class Basket extends StoreModule {
    * @param _id Код товара
    */
   addToBasket(_id) {
-    console.log('Here');
     let sum = 0;
     // Ищем товар в корзине, чтобы увеличить его количество
     let exist = false;
