a1597569cd579b04fc19083592964b1544a1c7eb
Eugene Lanets <swarrt@yandex.ru>
2023-06-01 19:48:19 +0300

    feat: get categories list from server

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,6 @@ import * as modules from './exports.js';
  * Хранилище состояния приложения
  */
 class Store {
-
   constructor(initState = {}) {
     this.listeners = []; // Слушатели изменений состояния
     this.state = initState;
@@ -31,8 +30,8 @@ class Store {
     this.listeners.push(listener);
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
   }
 
   /**
@@ -57,7 +56,7 @@ class Store {
     console.group(
       `%c${'store.setState'} %c${description}`,
       `color: ${'#777'}; font-weight: normal`,
-      `color: ${'#333'}; font-weight: bold`,
+      `color: ${'#333'}; font-weight: bold`
     );
     console.log(`%c${'prev:'}`, `color: ${'#d77332'}`, this.state);
     console.log(`%c${'next:'}`, `color: ${'#2fa827'}`, newState);
